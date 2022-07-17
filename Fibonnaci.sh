11ec242514123df9ca1e55d80e85c6f395148fa4
Vaibhav <mallikajangili1999@gmail.com>
2022-07-17 02:26:27 -0700

    Added solutions for Final review

---
+++ Fibonnaci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+echo "Enter the value of N: "
+
+read N
+
+sum=0
+a=0
+b=1
+
+echo "The Fibonacci series is : "
+
+
+while [ $sum -le $N ]
+do
+   a=$b
+   b=$sum
+   sum=$((a + b))
+   echo "$b"
+
+done
